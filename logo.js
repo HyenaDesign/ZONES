00163d75234a9cc7b9c8aaec234eb8baaa66360f
HyenaDesign <sam.hyenadesign@gmail.com>
2024-02-28 22:23:31 +0100

    LANDINGPAGE IN DEVELOPMENT

---
+++ logo.js
@@ -0,0 +1,7 @@
+document.getElementById('logo-img').addEventListener('mouseover', function() {
+  this.src = 'assets/alt-logo.svg';
+});
+
+document.getElementById('logo-img').addEventListener('mouseout', function() {
+  this.src = 'assets/Word-logo.svg';
+});
